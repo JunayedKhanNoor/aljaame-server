06a8d1b71d90ac6e95ef359bdc3acdcca9c5982f
163002029.junayed <junayednoor@gmail.com>
2022-07-16 16:01:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxkza.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db('aljaame').collection('products');
+
+    /**
+     * API Naming convention
+     * app.get('/products') //get all products in this collection
+     * app.delete('/products/:id') //delete product
+     */
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(filter);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello from Ajmaame Server');
+});
+app.listen(port, () => {
+  console.log(`Aljaame app listening on port: ${port}`);
+});
